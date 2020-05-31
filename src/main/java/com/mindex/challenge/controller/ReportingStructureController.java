d65418b12eeae6986a3b4506db113aaecac3d048
RohitKeswani <rhareshk@gmail.com>
2020-05-30 21:09:14 -0400

    implemented recursive finding of employee reports

--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -1,5 +1,6 @@
 package com.mindex.challenge.controller;
 
+import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
@@ -17,18 +18,46 @@ public class ReportingStructureController {
 
     @Autowired
     private ReportingStructureService reportingStructureService;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
 
     @GetMapping("/employee/reports/{id}")
     public ReportingStructure read(@PathVariable String id) {
         LOG.debug("Received employee reports request for id [{}]", id);
         Employee employee = reportingStructureService.read(id);
-        List<Employee> reports = employee.getDirectReports();
-        int numberOfReports = reports.size();
-        //TODO find number of reports of each employee and its employee recursively till you hit null.
+        // find number of reports of each employee and its employee recursively till you hit null.
+        int numberOfReports = findNumberOfReports(employee);
         ReportingStructure reportingStructure = new ReportingStructure();
         reportingStructure.setEmployee(employee);
-        reportingStructure.setNumberOfReports(numberOfReports);
+        reportingStructure.setNumberOfReports(numberOfReports-1);
         return reportingStructure;
+    }
+
+    private int findNumberOfReports(Employee startEmployee) {
+        if(startEmployee.getDirectReports() == null)
+            return 1;
+
+        int numberOfReports = 0;
+
+        //looping through all the direct reports of the employee
+        for(Employee employee:startEmployee.getDirectReports()) {
+
+            //the database call to fetch the employee
+            Employee emp = employeeRepository.findByEmployeeId(employee.getEmployeeId());
+            if (emp == null) {
+                //if employee is not present in the database
+                throw new RuntimeException("Invalid employeeId: " + employee.getEmployeeId());
+            }
+            employee.setFirstName(emp.getFirstName());
+            employee.setLastName(emp.getLastName());
+            employee.setPosition(emp.getPosition());
+            employee.setDepartment(emp.getDepartment());
+            employee.setDirectReports(emp.getDirectReports());
+
+            numberOfReports+= findNumberOfReports(emp);
+        }
+        return numberOfReports+1;
+
     }
 }
